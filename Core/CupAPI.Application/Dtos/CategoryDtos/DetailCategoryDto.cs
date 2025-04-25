b86d057fcc5afa6eed2bdac1393c8a36d26e8d1c
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-25 20:50:34 +0300

    v1

--- Core/CupAPI.Application/Dtos/CategoryDtos/DetailCategoryDto.cs
+++ Core/CupAPI.Application/Dtos/CategoryDtos/DetailCategoryDto.cs
@@ -7,5 +7,5 @@ public class DetailCategoryDto
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
 
-    public ICollection<ResultMenuItemDto> MenuItems { get; set; }
+    public ICollection<ResultMenuItemDto> MenuItems { get; set; } = null!;
 }
--- Core/CupAPI.Domain/Entities/User.cs
+++ Core/CupAPI.Domain/Entities/User.cs
@@ -8,5 +8,5 @@ public class User
     public string Surname { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
-    public string Role { get; set; }
+    public string Role { get; set; } = string.Empty;
 }
