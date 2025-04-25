da1d064608114169218be09adeedc58d31dbd3f8
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-04-26 01:08:40 +0300

    Mapping

---
+++ Core/CupAPI.Application/Mappings/CategoryMappings.cs
@@ -0,0 +1,20 @@
+﻿using CupAPI.Application.Dtos.CategoryDtos;
+using CupAPI.Domain.Entities;
+
+namespace CupAPI.Application.Mappings;
+
+public static class CategoryMappings
+{
+    public static DetailCategoryDto ToGetCategoryResult(this Category category)
+    {
+        return new()
+        {
+            Id = category.Id,
+            Name = category.Name
+        };
+    }
+}
+
+public static class MenuItemMappings
+{
+}
---
+++ Core/CupAPI.Application/Mappings/GeneralMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using CupAPI.Application.Dtos.CategoryDtos;
+using CupAPI.Application.Dtos.MenuItemDtos;
+using CupAPI.Domain.Entities;
+
+namespace CupAPI.Application.Mappings;
+
+internal class GeneralMapping : Profile
+{
+    public GeneralMapping()
+    {
+        CreateMap<Category, ResultCategoryDto>().ReverseMap();
+        CreateMap<Category, DetailCategoryDto>().ReverseMap();
+        CreateMap<Category, CreateCategoryDto>().ReverseMap();
+        CreateMap<Category, UpdateCategoryDto>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember is not null));
+
+        CreateMap<MenuItem, ResultMenuItemDto>().ReverseMap();
+        CreateMap<MenuItem, DetailMenuItemDto>().ReverseMap();
+        CreateMap<MenuItem, CreateMenuItemDto>().ReverseMap();
+        CreateMap<MenuItem, UpdateMenuItemDto>().ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember is not null));
+    }
+}
