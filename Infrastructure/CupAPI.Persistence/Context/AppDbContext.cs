8e4f247fecfdba69d9b977d1da24037a2dcefc5e
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-05-05 04:19:39 +0300

    Table eklendi

--- Infrastructure/CupAPI.Persistence/Context/AppDbContext.cs
+++ Infrastructure/CupAPI.Persistence/Context/AppDbContext.cs
@@ -5,11 +5,10 @@ namespace CupAPI.Persistence.Context;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Category> Categories => Set<Category>();
     public DbSet<MenuItem> MenuItems => Set<MenuItem>();
+    public DbSet<Table> Tables => Set<Table>();
     public DbSet<User> Users => Set<User>();
 }
