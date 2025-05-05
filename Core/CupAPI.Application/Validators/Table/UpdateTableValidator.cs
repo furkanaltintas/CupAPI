6d280acb39e83e20c47d53f3f3c6cd7f08202c2f
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-05-05 18:20:35 +0300

    Table için validasyon işlemleri yapıldı

---
+++ Core/CupAPI.Application/Validators/Table/UpdateTableValidator.cs
@@ -0,0 +1,26 @@
+﻿using CupAPI.Application.Dtos.TableDtos;
+using FluentValidation;
+
+namespace CupAPI.Application.Validators.Table;
+
+public sealed class UpdateTableValidator : AbstractValidator<UpdateTableDto>
+{
+    public UpdateTableValidator()
+    {
+        RuleFor(x => x.Id).GreaterThan(0).WithMessage("Id sıfırdan büyük olmalıdır.");
+
+        // Type enum değerinin geçerli olduğundan emin olun
+        RuleFor(x => x.Type).IsInEnum().WithMessage("Geçersiz masa türü.");
+
+        // TableCode boş olamaz
+        RuleFor(x => x.TableCode)
+            .NotEmpty().WithMessage("Masa kodu boş olamaz.")
+            .Length(3, 50).WithMessage("Masa kodu 3 ile 50 karakter arasında olmalıdır.");
+
+        // TableNumber pozitif bir sayı olmalı
+        RuleFor(x => x.TableNumber).GreaterThan(0).WithMessage("Masa numarası sıfırdan büyük olmalıdır.");
+
+        // Capacity pozitif bir sayı olmalı
+        RuleFor(x => x.Capacity).GreaterThan(0).WithMessage("Kapasite sıfırdan büyük olmalıdır.");
+    }
+}
