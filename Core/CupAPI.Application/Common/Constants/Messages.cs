c9789ccf47c15af2f3f1ea8796e66c4cbed10f38
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-05-05 04:16:29 +0300

    Sabit mesajlar hazırlandı

---
+++ Core/CupAPI.Application/Common/Constants/Messages.cs
@@ -0,0 +1,45 @@
+﻿namespace CupAPI.Application.Common.Constants;
+
+public static class Messages
+{
+    public static class Category
+    {
+        public const string NotFound = "Kategori bulunamadı";
+        public const string Created = "Kategori oluşturuldu";
+        public const string Updated = "Kategori güncellendi";
+        public const string Deleted = "Kategori silindi";
+        public const string ErrorWhileAdding = "Kategori eklenirken bir hata oluştu";
+        public const string ErrorWhileUpdating = "Kategori güncellenirken bir hata oluştu";
+        public const string ErrorWhileDeleting = "Kategori silinirken bir hata oluştu";
+        public const string ErrorWhileFetching = "Kategori getirilirken bir hata oluştu";
+    }
+
+    public static class MenuItem
+    {
+        public const string NotFound = "Menü bulunamadı";
+        public const string Created = "Menü oluşturuldu";
+        public const string Updated = "Menü güncellendi";
+        public const string Deleted = "Menü silindi";
+        public const string ErrorWhileAdding = "Menü eklenirken bir hata oluştu";
+        public const string ErrorWhileUpdating = "Menü güncellenirken bir hata oluştu";
+        public const string ErrorWhileDeleting = "Menü silinirken bir hata oluştu";
+        public const string ErrorWhileFetching = "Menü getirilirken bir hata oluştu";
+    }
+
+    public static class Table
+    {
+        public const string NotFound = "Masa bulunamadı";
+        public const string Created = "Masa oluşturuldu";
+        public const string Updated = "Masa güncellendi";
+        public const string Deleted = "Masa silindi";
+        public const string ErrorWhileAdding = "Masa eklenirken bir hata oluştu";
+        public const string ErrorWhileUpdating = "Masa güncellenirken bir hata oluştu";
+        public const string ErrorWhileDeleting = "Masa silinirken bir hata oluştu";
+        public const string ErrorWhileFetching = "Masa getirilirken bir hata oluştu";
+    }
+
+    public static class General
+    {
+        public const string UnexpectedError = "Beklenmeyen bir hata oluştu";
+    }
+}
